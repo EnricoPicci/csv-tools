48dfe28234939cb26faa8fdc37bcdc17562f8981
Enrico PIccinin <enrico.piccinin@gmail.com>
2023-06-05 08:39:12 +0200

    1.0.8

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.toCsvObs = exports.toCsv = exports.fromCsv = void 0;
+exports.toCsvObs = exports.toCsv = exports.fromCsvObs = exports.fromCsvArray = exports.fromCsv = void 0;
 const from_csv_1 = require("./from-csv");
 Object.defineProperty(exports, "fromCsv", { enumerable: true, get: function () { return from_csv_1.fromCsv; } });
+Object.defineProperty(exports, "fromCsvArray", { enumerable: true, get: function () { return from_csv_1.fromCsvArray; } });
+Object.defineProperty(exports, "fromCsvObs", { enumerable: true, get: function () { return from_csv_1.fromCsvObs; } });
 const to_csv_1 = require("./to-csv");
 Object.defineProperty(exports, "toCsv", { enumerable: true, get: function () { return to_csv_1.toCsv; } });
 Object.defineProperty(exports, "toCsvObs", { enumerable: true, get: function () { return to_csv_1.toCsvObs; } });
