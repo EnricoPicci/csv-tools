f95e2c0cd685cc49a6304261c254b6e00b6ce717
Enrico PIccinin <enrico.piccinin@gmail.com>
2023-06-05 08:35:25 +0200

    fromCsvObs added and fromCsv typed

--- src/from-csv.spec.ts
+++ src/from-csv.spec.ts
@@ -1,5 +1,8 @@
 import { expect } from 'chai';
-import { fromCsv } from './from-csv';
+
+import { from, toArray } from 'rxjs';
+
+import { fromCsv, fromCsvArray, fromCsvObs } from './from-csv';
 import { DEFAUL_CONFIG } from './config';
 
 describe(`fromCsv`, () => {
@@ -8,9 +11,9 @@ describe(`fromCsv`, () => {
         const header = `col_1${sep} col_2${sep} col_3`;
         const lines = [`1${sep}2${sep}3`, `a${sep}b${sep}c`];
 
-        type objType = { col_1: string; col_2: string; col_3: string };
+        type ObjType = { col_1: string; col_2: string; col_3: string };
 
-        const objsFromCsv = fromCsv(header, lines) as objType[];
+        const objsFromCsv = fromCsv<ObjType>(header, lines);
 
         expect(objsFromCsv.length).equal(lines.length);
         const firstObj = objsFromCsv[0];
@@ -20,5 +23,53 @@ describe(`fromCsv`, () => {
         const lastObj = objsFromCsv[lines.length - 1];
         expect(lastObj.col_2).equal('b');
         expect(lastObj.col_3).equal('c');
+    });
+});
+
+describe(`fromCsvArray`, () => {
+    it(`create an array of objects starting from an array of lines representing csv records`, () => {
+        const sep = DEFAUL_CONFIG.CSV_SEP;
+        const header = `col_1${sep} col_2${sep} col_3`;
+        const lines = [header, `1${sep}2${sep}3`, `a${sep}b${sep}c`];
+
+        type ObjType = { col_1: string; col_2: string; col_3: string };
+
+        const objsFromCsv = fromCsvArray<ObjType>(lines);
+
+        expect(objsFromCsv.length).equal(lines.length - 1);
+        const firstObj = objsFromCsv[0];
+        expect(firstObj.col_1).equal('1');
+        expect(firstObj.col_2).equal('2');
+        expect(firstObj.col_3).equal('3');
+        const lastObj = objsFromCsv[lines.length - 2];
+        expect(lastObj.col_2).equal('b');
+        expect(lastObj.col_3).equal('c');
+    });
+});
+
+describe(`fromCsvObs`, () => {
+    it(`create an array of objects starting from a stream of lines representing csv records`, (done) => {
+        const sep = DEFAUL_CONFIG.CSV_SEP;
+        const header = `col_1${sep} col_2${sep} col_3`;
+        const lines = [header, `1${sep}2${sep}3`, `a${sep}b${sep}c`];
+
+        type ObjType = { col_1: string; col_2: string; col_3: string };
+
+        from(lines)
+            .pipe(fromCsvObs<ObjType>(), toArray())
+            .subscribe({
+                next: (objsFromCsv) => {
+                    expect(objsFromCsv.length).equal(lines.length - 1);
+                    const firstObj = objsFromCsv[0];
+                    expect(firstObj.col_1).equal('1');
+                    expect(firstObj.col_2).equal('2');
+                    expect(firstObj.col_3).equal('3');
+                    const lastObj = objsFromCsv[lines.length - 2];
+                    expect(lastObj.col_2).equal('b');
+                    expect(lastObj.col_3).equal('c');
+                },
+                error: (err) => done(err),
+                complete: () => done(),
+            });
     });
 });
--- src/from-csv.ts
+++ src/from-csv.ts
@@ -1,18 +1,64 @@
+import { Observable, Subscriber } from 'rxjs';
 import { DEFAUL_CONFIG } from './config';
 
-export function fromCsv(header: string, lines: string[], csvSep = DEFAUL_CONFIG.CSV_SEP) {
+export function fromCsv<T>(header: string, lines: string[], csvSep = DEFAUL_CONFIG.CSV_SEP) {
     const columns = header.split(csvSep);
     return lines.map((line, i) => {
-        const cells = line.split(csvSep);
-        if (columns.length !== cells.length) {
-            throw new Error(
-                `The number of cells in line number ${i} ("${line}" is not the same as the number of columns specified in the header "${header})"`,
-            );
-        }
-        return cells.reduce((obj, cell, i) => {
-            obj[columns[i].trim()] = cell;
-            return obj;
-            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        }, {} as any);
+        return fromCsvRec<T>(columns, line, csvSep, i);
     });
+}
+
+export function fromCsvArray<T>(lines: string[], csvSep = DEFAUL_CONFIG.CSV_SEP) {
+    if (lines.length < 2) {
+        return [] as T[];
+    }
+    const header = lines[0];
+    return fromCsv<T>(header, lines.slice(1), csvSep);
+}
+
+// Custom operator which uses the first line notified by upstream to build the header and the following lines to emit
+// objects representing the csv records.
+// https://rxjs.dev/guide/operators#creating-new-operators-from-scratch
+export function fromCsvObs<T>(csvSep = DEFAUL_CONFIG.CSV_SEP) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (source: Observable<string>) => {
+        let isFirst = true;
+        let columns: string[];
+        let index = 0;
+        return new Observable((subscriber: Subscriber<T>) => {
+            const subscription = source.subscribe({
+                next: (csvRec) => {
+                    if (isFirst) {
+                        isFirst = false;
+                        columns = csvRec.split(csvSep);
+                        return;
+                    }
+                    const obj = fromCsvRec<T>(columns, csvRec, csvSep, index++);
+                    subscriber.next(obj);
+                },
+                error: (err) => subscriber.error(err),
+                complete: () => {
+                    subscriber.complete();
+                },
+            });
+            return () => {
+                subscription.unsubscribe();
+            };
+        });
+    };
+}
+
+function fromCsvRec<T>(columns: string[], csvRec: string, csvSep: string, index: number) {
+    const cells = csvRec.split(csvSep);
+    if (columns.length !== cells.length) {
+        throw new Error(
+            `The number of cells in line number ${index} ("${csvRec}" is not the same as the number of columns 
+                specified in the header "${columns.join(csvSep)})"`,
+        );
+    }
+    return cells.reduce((obj, cell, i) => {
+        obj[columns[i].trim()] = cell;
+        return obj;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    }, {} as any) as T;
 }
